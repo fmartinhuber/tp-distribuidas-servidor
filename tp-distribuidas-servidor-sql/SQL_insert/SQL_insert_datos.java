a11bef20194c5a5f4c636b563bd003a6ce0ed3f5
Ramiro <Ramiro@10.100.110.72>
2015-06-01 19:20:24 -0300

    Cambie el script del SQL

--- tp-distribuidas-servidor-sql/SQL_insert/SQL_insert_datos.java
+++ tp-distribuidas-servidor-sql/SQL_insert/SQL_insert_datos.java
@@ -1,5 +1,10 @@
 /*
  * Inserts usados para tener datos precargados en la base de datos
  * Correr el proyecto por 1era vez con create-drop, despues ponerlo como validate y correr estos insert en la BD 
+ * 
+ * 
+ * 
+ * 
+ * 
 */
 
