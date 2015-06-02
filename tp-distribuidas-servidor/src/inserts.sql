b0d3a36231e874018076f55c46cce7a3c866c4bc
Daro <argiesdario@gmail.com>
2015-06-02 13:10:55 -0300

    Cargando inserts

--- tp-distribuidas-servidor/src/inserts.sql
+++ tp-distribuidas-servidor/src/inserts.sql
@@ -1,1 +1,22 @@
-asdf
+--Se crea este archivo para tener todos los inserts para la BD. By Daro
+--1) Corran el Servidor en create and drop
+--2) Copien y pegen estos inserts en la BD
+--3) Cambien la configuracion del Servidor a validate
+--Y ya tienen datos para trabajar con consultas hql
+
+--Dimension
+insert into Dimension (alto, ancho, peso, profundidad, volumen)
+values (10, 4, 1500, 15, 3500)
+go
+insert into Dimension (alto, ancho, peso, profundidad, volumen)
+values (8, 3, 750, 12, 1600)
+go
+insert into Dimension (alto, ancho, peso, profundidad, volumen)
+values (11, 4, 1660, 14, 2000)
+go
+insert into Dimension (alto, ancho, peso, profundidad, volumen)
+values (10, 4, 2100, 20, 4500)
+go
+
+
+--
